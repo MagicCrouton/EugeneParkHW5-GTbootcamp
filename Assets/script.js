9b35f3fc87187d4c0b622a3ddb82e04ea823eb70
Eugene Park <e.park5336@gmail.com>
2022-11-27 23:31:35 -0500

    got the core working to meet acceptance criteria

--- Assets/script.js
+++ Assets/script.js
@@ -2,15 +2,18 @@ var calendarEl = $('#container');
 var currentDayEl = $('#currentDay');
 var dayStart = 7;
 var dayEnd = 17;
+var businessHours = [7,8,9,10,11,12,13,14,15,16,17];
+
 // put current time variable up here so i can test the code easily.
 // var currentTime = parseInt(moment().format('HH'));
-var currentTime = 11;
+var currentTime = 3;
 
 var dateTimeCounter = setInterval(function(){
     currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'))
 }, 1000)
 
-var businessHours = [7,8,9,10,11,12,13,14,15,16,17]
+
+
 businessHours.forEach(element => {
     if (element<12) {
         var time = `${element}AM`;
@@ -32,11 +35,11 @@ businessHours.forEach(element => {
     else if (element === currentTime) {
     calendarEl.append(`<ul class="list-group list-group-horizontal">
                     <li class="list-group-item list-group-item-warning timeGroup col-2 col-md-2 col-lg-1">${time}</li>
-                    <li id = "${time}TimeGroupList" class="list-group-item list-group-item-warning timeGroup col-8 col-md-8 col-lg-10">
-                        <input class="form-control form-control-lg" type="text" placeholder="Input Task here" aria-label=".form-control-lg example">
+                    <li id = "${time}TimeGroupContainer" class="list-group-item list-group-item-warning timeGroup col-8 col-md-8 col-lg-10">
+                        <input id = "${time}TimeGroupList" class="form-control form-control-lg" type="text" placeholder="Input Task here" aria-label=".form-control-lg example">
                     </li>
                     <li class="list-group-item list-group-item-warning col-2 col-md-2 col-lg-1">
-                        <button id = "${time}timeGroup" type="button" class="btn btn-primary"><img src="./Assets/schedule.png" alt="save button">Save</button>
+                        <button id = "${time}TimeGroup" type="button" class="btn btn-primary"><img src="./Assets/schedule.png" alt="save button">Save</button>
                     </li>
                     </ul>`);
                     //got the button from https://www.flaticon.com/free-icon/schedule_3774487?related_id=3774487&origin=search#
@@ -44,20 +47,72 @@ businessHours.forEach(element => {
     else {
     calendarEl.append(`<ul class="list-group list-group-horizontal">
                     <li class="list-group-item list-group-item-primary timeGroup col-2 col-md-2 col-lg-1">${time}</li>
-                    <li id = "${time}TimeGroupList" class="list-group-item list-group-item-primary timeGroup col-8 col-md-8 col-lg-10">
-                        <input class="form-control form-control-lg" type="text" placeholder="Input Task here" aria-label=".form-control-lg example">
+                    <li id = "${time}TimeGroupContainer" class="list-group-item list-group-item-primary timeGroup col-8 col-md-8 col-lg-10">
+                        <input id = "${time}TimeGroupList" class="form-control form-control-lg" type="text" placeholder="Input Task here" aria-label=".form-control-lg example">
                     </li>
                     <li class="list-group-item list-group-item-primary col-2 col-md-2 col-lg-1">
-                        <button id = "${time}timeGroup" type="button" class="btn btn-primary"><img src="./Assets/schedule.png" alt="save button">Save</button>
+                        <button id = "${time}TimeGroup" type="button" class="btn btn-primary"><img src="./Assets/schedule.png" alt="save button">Save</button>
                     </li>
                     </ul>`);
     }
 
 });
 
-var modifyTimeWindow = document.querySelectorAll('.btn');
+// see's if there is existing local data and if not it will generate an array of objects for the planner data
+if (window.localStorage.getItem('plannerData') === null) {
 
+    var plan = [];
+    businessHours.forEach(hour => {
+        if (hour < 12)
+        plan[hour-businessHours[0]] = {
+            time: `${hour}AM`,
+            toDo: []
+        }
+        else if (hour === 12)
+        plan[hour-businessHours[0]] = {
+            time: `${hour}PM`,
+            toDo: []
+        }
+        else {
+        plan[hour-businessHours[0]] = {
+            time: `${hour-12}PM`,
+            toDo: []    
+        }      
+    }})
+
+}
+
+else {
+var plan = JSON.parse(window.localStorage.getItem('plannerData'));
+plan.forEach(element => {
+    var tempContainer = $(`#${element.time}TimeGroupContainer`);
+    for (i=0; i< element.toDo.length; ++i) {
+        tempContainer.append(`
+        <li class="list-group-item list-group-item-info timeGroup col-8 col-md-8 col-lg-10">${element.toDo[i]}
+        </li>`)
+    }
+})}
+
+
+var modifyTimeWindow = document.querySelectorAll('.btn');
 modifyTimeWindow.forEach(element => {
     element.addEventListener('click', function() {
-        console.log(`${element.id} was clicked`);
-})})
+        var temp = document.getElementById(`${element.id}List`).value
+        console.log(temp)
+        var tempContainer = $(`#${element.id}Container`);
+        if (temp === '') {
+
+        }
+
+        else {
+        tempContainer.append(`
+            <li class="list-group-item list-group-item-info timeGroup col-8 col-md-8 col-lg-10">${temp}
+            </li>`)
+        console.log(element.id)
+        // this pushes the task into the array of the plan object i used slice because i didn't want to refactor my whole code again.
+        var index = plan.map(object => object.time).indexOf(`${element.id.slice(0,-9)}`);
+        plan[index].toDo.push(`${temp}`);
+        window.localStorage.setItem('plannerData', JSON.stringify(plan));
+        document.getElementById(`${element.id}List`).value = ('');
+        }
+    })})
